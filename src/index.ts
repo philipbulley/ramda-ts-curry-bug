f7b958971e69cbdcc03ffd5ae1cb2072004dba9c
Philip Bulley <philip@milkisevil.com>
2020-06-03 19:24:52 +0100

    add comment

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ const person3 = curry(person);
 
 let person1: F.Curry<(happy: boolean) => {age: number, name: string, happy: boolean}>;
 
-person1 = person3(99, 'Bob');
+person1 = person3(99, 'Bob');   // tsc doesn't like this assignment "Excessive stack depth", assuming that checking the types match is too much for tsc to handle??
 
 console.log(person1(true));   // {age: 99, name: 'Bob', happy: true}
